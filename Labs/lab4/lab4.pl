d2d92de36b7bb9b86caf4b17f633cd019191f43a
Qasimtalkin <abulqasim110@gmail.com>
2019-02-08 17:50:33 -0500

    lab4 answer

--- Labs/lab4/lab4.pl
+++ Labs/lab4/lab4.pl
@@ -1,49 +1,46 @@
-Consider a small database that represents books, readers and loans.
+% Decision to go skiing
 
-% --------
-% book( Title, Authors, Publisher, Year, CallNumber )
-% --------
+% trail conditions
+condition( skyline, green ).
+condition( burma, green ).
+condition( fortune, green ).
+condition( mcclosky, red ).
+    
+% no time tuesday thursday CSI2120
+available( monday ).
+available( wednesday ).
+available( friday ).
+    
+% high temperatures
+temperature( mild, monday ).
+temperature( mild, tuesday ).
+temperature( verycold, wednesday ).
+temperature( cold, thursday ).
+temperature( warm, friday ).
 
-book(
-  'The craft of Prolog',
-  'R. A. O''Keefe',
-  'MIT Press',
-  1990,
-  'QA 76.73 .P76 O38 1990'
-).
-book(
-  'Programming in Prolog',
-  'W. F. Clocksin, C. S. Mellish',
-  'Springer',
-  2003,
-  'QA 76.73 .P76 C57 2003'
-).
-book(
-  'Prolog for programmers',
-  'F. Kluzniak, S. Szpakowicz',
-  'Academic Press',
-  1985,
-  'QA 76.73 .P76 K58 1985'
-).
-book(
-  'Prolog programming for artificial intelligence',
-  'I. Bratko',
-  'Addison-Wesley',
-  2001,
-  'Q 336 .B74 2001'
-).
+% available wax  
+wax( green, verycold ).
+wax( blueExtra, mild ).
+wax( red, warm ).
 
-% --------
-% reader( CardNumber, Name )
-% --------
+route( Col ) :- condition( skyline, Col ),
+		condition( burma, Col ),
+		condition( fortune, Col ).
 
-reader( 1234567, 'James Brown' ).
-reader( 2345678, 'Jacques Brun' ).
-reader( 3456789, 'Giacomo Bruno' ).
+% hard-wired lights
+connected(L) :- fuse(L, F),
+		ok(F).
 
-% --------
-% checkedOut( CardNumber, CallNumber )
-% --------
+% plugged in ligh
+connected(L) :- plug(L,S),
+                fuse(S,F),
+                ok(F).
 
-checkedOut( 1234567, 'QA 76.73 .P76 C57 2003' ).
-checkedOut( 3456789, 'Q 336 .B74 2001' ).
+
+
+%Answer Q3            
+goSki(D,W) :- 
+    available( D ),
+    temperature( mild, D ),
+    wax( W, mild ),
+    condition( burma, green ).
